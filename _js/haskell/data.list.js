de75e0ff93128b5657321df73941979196a90744
Jesse Mykolyn <jrmykolyn@gmail.com>
2020-09-08 16:43:47 -0400

    Add tail() and init()

--- _js/haskell/data.list.js
+++ _js/haskell/data.list.js
@@ -4,7 +4,15 @@ const head = ([h]) => h;
 // last :: [a] -> a
 const last = (arr) => arr[arr.length - 1];
 
+// tail :: [a] -> [a]
+const tail = ([_, ...t]) => t;
+
+// init :: [a] -> [a]
+const init = (arr) => arr.slice(0, arr.length - 1);
+
 module.exports = {
   head,
   last,
+  tail,
+  init,
 };
