0d4070e1a55441ebec7d57a81232f69a4a86edd3
Jesse Mykolyn <jrmykolyn@gmail.com>
2021-07-11 16:36:08 -0400

    Implement Recaman's Sequence; add tests

--- _js/recamans-sequence/index.js
+++ _js/recamans-sequence/index.js
@@ -9,3 +9,19 @@
  * - If (n - 1) minus the position yields a number that is already present in the sequence, then the value of the position is equal to (n - 1) plus the position.
  * - Otherwise the value of the position is equal to (n - 1) minus the position.
  */
+const recamansSequence = (n, cache = {}) => {
+  if (!n) return 0;
+  const prev = recamansSequence(n - 1, cache);
+  const tmp = prev - n;
+  const next = tmp > 0 && !(tmp in cache) ? tmp : prev + n;
+  cache[next] = true;
+  return next;
+};
+
+console.log(recamansSequence(0) === 0);
+console.log(recamansSequence(1) === 1);
+console.log(recamansSequence(2) === 3);
+console.log(recamansSequence(3) === 6);
+console.log(recamansSequence(4) === 2);
+console.log(recamansSequence(5) === 7);
+console.log(recamansSequence(6) === 13);
