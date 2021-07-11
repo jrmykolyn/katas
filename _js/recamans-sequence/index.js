25d4412f2b034aa7f9155ca141693bef98a120f4
Jesse Mykolyn <jrmykolyn@gmail.com>
2021-07-11 16:44:07 -0400

    Add examples for Recaman's Sequence

--- _js/recamans-sequence/index.js
+++ _js/recamans-sequence/index.js
@@ -8,6 +8,26 @@
  * - If (n - 1) minus the position yields a negative number, then the value of the posiiton is equal to (n - 1) plus the position.
  * - If (n - 1) minus the position yields a number that is already present in the sequence, then the value of the position is equal to (n - 1) plus the position.
  * - Otherwise the value of the position is equal to (n - 1) minus the position.
+ *
+ * Examples:
+ *
+ * Computing the value at position 1:
+ * - First, retrieve the value for position 0 (eg. 1 - 1).
+ * - The value for position 0 is 0.
+ * - Then subtract the current position (eg. 1) from the retrieved value: 0 - 1.
+ * - This gives us a value of -1.
+ * - Since the numbers in Recaman's Sequence cannot be negative, we instead add the previously retrieved value to the current position: 0 + 1.
+ * - This gives us a value of 1.
+ * - The value for position 1 is 1.
+ *
+ * Computing the value at position 6:
+ * - First, retrieve the value for position 5 (eg. 6 - 1).
+ * - The value for position 5 is 7.
+ * - Then subtrac the current position (eg. 6) from the retrieved value: 7 - 1.
+ * - This gives us a value of 1.
+ * - Although 1 is a positive number, it is already present in the sequence.
+ * - As a result we add the previosly retrieved value to the current position: 7 + 6.
+ * - The value for position 6 is 13.
  */
 const recamansSequence = (n, cache = {}) => {
   if (!n) return 0;
